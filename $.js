fea56787f208eafcea84e05dd122ba052cabe42e
asher0122499 <55705051+asher0122499@users.noreply.github.com>
2019-10-30 19:22:56 -0400

    A

--- $.js
+++ $.js
@@ -1,5 +1,5 @@
     $(function () {
-        $(":file").change(function () {
+        $("file1").change(function () {
             if (this.files && this.files[0]) {
                 var reader = new FileReader();
 
@@ -10,7 +10,41 @@
     });
 
     function imageIsLoaded(e) {
-        $('#myImg').attr('src', e.target.result);
-        $('#yourImage').attr('src', e.target.result);
+        $('image1').attr('src', e.target.result);
+        $('image1').attr('src', e.target.result);
+        
+    };
+
+        $(function () {
+        $("file2").change(function () {
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = imageIsLoaded;
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+    });
+
+    function imageIsLoaded(e) {
+        $('image2').attr('src', e.target.result);
+        $('image2').attr('src', e.target.result);
+        
+    };
+
+        $(function () {
+        $("file2").change(function () {
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = imageIsLoaded;
+                reader.readAsDataURL(this.files[0]);
+            }
+        });
+    });
+
+    function imageIsLoaded(e) {
+        $('image2').attr('src', e.target.result);
+        $('image2').attr('src', e.target.result);
         
     };
--- ini.php
+++ ini.php
@@ -1,11 +1,69 @@
 <?php
 file_uploads=On
-   if(isset($_FILES['image'])){
+   if(isset($_FILES['image1'])){
       $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
+      $file_name = $_FILES['image1']['name'];
+      $file_size =$_FILES['image1']['size'];
+      $file_tmp =$_FILES['image1']['tmp_name'];
+      $file_type=$_FILES['image1']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png","gif");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be exactly 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+
+<?php
+file_uploads=On
+   if(isset($_FILES['image2'])){
+      $errors= array();
+      $file_name = $_FILES['image2']['name'];
+      $file_size =$_FILES['image2']['size'];
+      $file_tmp =$_FILES['image2']['tmp_name'];
+      $file_type=$_FILES['image2']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png","gif");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be exactly 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+
+<?php
+file_uploads=On
+   if(isset($_FILES['image3'])){
+      $errors= array();
+      $file_name = $_FILES['image3']['name'];
+      $file_size =$_FILES['image3']['size'];
+      $file_tmp =$_FILES['image3']['tmp_name'];
+      $file_type=$_FILES['image3']['type'];
       $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
       
       $extensions= array("jpeg","jpg","png","gif");
